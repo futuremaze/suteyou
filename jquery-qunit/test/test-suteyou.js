9cba8e9956ea3bad8c9c8c1dfb33e7ed32cdfcb9
futuremaze <future.maze@gmail.com>
2012-08-20 17:50:23 +0900

    [ADD] test QUnitの登録。

---
+++ jquery-qunit/test/test-suteyou.js
@@ -0,0 +1,18 @@
+function startSuteyoujsTest() {
+	var s = new Suteyou();
+	var fileData = document.getElementById("inputFile").files[0];
+
+	module('read file(text mode)', {
+	  setup: function() {
+	    // setup for start test to read a file(text mode) 
+	  },
+	  teardown: function() {
+	    //teardown for start test to read a file(text mode)
+	  }
+	});
+	
+	test('Suteyou.open', 1, function() {
+	  var reader = s.open(fileData, "r");
+	  equal(reader, !undefined, "Success s.open");
+	});
+}
